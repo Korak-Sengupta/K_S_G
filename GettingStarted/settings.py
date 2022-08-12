02c71944264dc5d4fafe32c8400995510111b43d
Korak <senguptakorak38@gmail.com>
2022-08-12 15:32:55 +0530

    3rd commit

--- GettingStarted/settings.py
+++ GettingStarted/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-tr+p!tqh-bqn#7khsbm+2a)49)yl#019kuk@9w*w(s!*kn^f3+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','portfolio-ksg.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
