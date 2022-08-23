96883ca7a6edb37d479f832332949ae6cfc99b1d
Abhishek Sengupta <106603514+silverfruitplayer@users.noreply.github.com>
2022-08-23 21:25:32 +0530

    Update settings.py

--- django_start/GettingStarted/settings.py
+++ django_start/GettingStarted/settings.py
@@ -51,6 +51,23 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = (
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+     
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static")     
+     
 ROOT_URLCONF = 'GettingStarted.urls'
 
 
